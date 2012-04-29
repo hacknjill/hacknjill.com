689644d948ff6de2db04be9afc9c9ea2f6b9f392
judyhe <judy.w.he@gmail.com>
2012-04-29 17:13:23 -0400

    start a new hacknjill wp theme

---
+++ blog/wp-content/themes/hacknjill/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #content -->
+
+  <footer id="site-footer" class="container">
+    <div id="contact-wrapper">
+      <div id="contact">
+        <h4>Get Involved!</h4>
+        <p>
+          We are looking for hackers, sponsors, and volunteers. <br />
+          Connect with us: <a href="http://twitter.com/hacknjill">@hacknjill</a> or <a href="mailto:hi@hacknjill.com">email</a></p>
+      </div>
+      <div class="dots"></div>        
+    </div>
+    <div id="footer-branding">
+      Hack'n Jill
+      <div class="dots"></div>
+    </div>
+
+  </footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
