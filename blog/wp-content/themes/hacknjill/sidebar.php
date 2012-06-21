a7079655e089e1985f21122570b799c6335bace1
judyhe <judy.w.he@gmail.com>
2012-06-20 22:06:16 -0400

    take out sign up links

--- blog/wp-content/themes/hacknjill/sidebar.php
+++ blog/wp-content/themes/hacknjill/sidebar.php
@@ -10,14 +10,11 @@
 
 <div class="span5">
   
-  <a href="http://hacknjill.eventbrite.com/" class="signup">
   <div id="signup" class="dots-block">
     <h3>#HackYourSummer</h3>
     <h2>June 15-16, 2012</h2>
-    <div class="button">sign up</div>
     <div class="dots"></div>
   </div>
-  </a>
   
   <div id="sidebar">
     <?php dynamic_sidebar( 'sidebar-1' ) ?>
