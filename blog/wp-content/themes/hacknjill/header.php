f8490ed315f835ad9b7063cbbfebdc4b93b025b3
judyhe <judy.w.he@gmail.com>
2013-09-02 18:01:38 -0400

    links to bnp

--- blog/wp-content/themes/hacknjill/header.php
+++ blog/wp-content/themes/hacknjill/header.php
@@ -46,7 +46,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="/css/hacknjill-022413.css" />
+<link rel="stylesheet" type="text/css" media="all" href="/css/hacknjill-080213.css" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
